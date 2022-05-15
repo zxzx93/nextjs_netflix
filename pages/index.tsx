4c67313acfe978c9e792d485c5170bb1227f7810
zxzx93 <thddustn26@hanmail.net>
2022-05-15 19:15:37 +0900

    모달창 열렸을때 메인 페이지 스크롤 불가능 하도록 함

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
-import { useRecoilState } from 'recoil'
+import { useRecoilValue } from 'recoil'
 import { modalState } from '../atoms/modalAtom'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
@@ -9,7 +8,7 @@ import requests from '../utils/requests'
 import { Movie } from '../typings'
 import Row from '../components/Row'
 import useAuth from '../hooks/useAuth'
-import Modal from '../components/Modal';
+import Modal from '../components/Modal'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -33,12 +32,16 @@ const Home = ({
   documentaries,
 }: Props) => {
   const { loading } = useAuth()
-  const showModal = useRecoilState(modalState)
+  const showModal = useRecoilValue(modalState)
 
-  if (loading) 'Loading....'
+  if (loading) return null
 
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
